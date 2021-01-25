73ecb162ad468360fc301c425bfcc71b2fee3b05
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-25 15:33:24 -0300

    implementando a API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\PostControlador;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', PostControlador::class, 'index');
+Route::post('/', PostControlador::class, 'store');
+Route::delete('/{id}', PostControlador::class, 'destroy');
+Route::get('/like/{id}', PostControlador::class, 'like');
