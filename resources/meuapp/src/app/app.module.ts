7551244df49bc0c2d9529f27403ca0feb0c79a6f
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-25 09:27:37 -0300

    instalacao do Angular Material framework

--- resources/meuapp/src/app/app.module.ts
+++ resources/meuapp/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
         <title>Laravel</title>
     </head>
     <body>
         
         <app-root></app-root>
-        
+
         <script src="{{ asset('js/runtime.js') }}" defer></script>
         <script src="{{ asset('js/polyfills.js') }}" defer></script>
         <script src="{{ asset('js/vendor.js') }}" defer></script>
