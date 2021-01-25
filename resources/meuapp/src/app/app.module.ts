634015399e274fc235a378af06f8908aba721a8f
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-25 10:28:19 -0300

    pagina principal

--- resources/meuapp/src/app/app.module.ts
+++ resources/meuapp/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
 import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +26,22 @@ import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+
     MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatBadgeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
