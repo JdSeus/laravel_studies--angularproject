37cd497af179d95e1ced6c3e98831fe0c2b7661f
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-26 08:32:20 -0300

    buscando posts

--- resources/meuapp/src/app/post.service.ts
+++ resources/meuapp/src/app/post.service.ts
@@ -7,20 +7,18 @@ import { Post } from './post';
 })
 export class PostService {
 
-  public posts: Post[] = [
-    new Post("Joao", "Meu Post", "Sub Joao", "joao@gmail.com", "Minha msg"),
-    new Post("Paulo", "Posto do Paulo", "Sub Paulo", "paulo@gmail.com", "Msg do Paulo"),
-    new Post("Maria", "Post da Maria", "Sub Maria", "maria@gmail.com", "Msg da Maria"),
-    new Post("Carlos", "Meu Post", "Sub Carlos", "Carlos@gmail.com", "Minha msg Carlos"),
-    new Post("Eduardo", "Posto do Eduardo", "Sub Eduardo", "Eduardo@gmail.com", "Msg do Eduardo"),
-    new Post("Tania", "Post da Tania", "Sub Tania", "Tania@gmail.com", "Msg da Tania"),
-    new Post("Kadu", "Meu Post", "Sub Kadu", "Kadu@gmail.com", "Minha msg"),
-    new Post("Tomas", "Posto do Tomas", "Sub Tomas", "Tomas@gmail.com", "Msg do Tomas"),
-    new Post("Alice", "Post da Alice", "Sub Alice", "Alice@gmail.com", "Msg da Alice"),
-  ];
+  public posts: Post[] = [];
 
   constructor(private http: HttpClient) {
-    
+    this.http.get("/api/").subscribe(
+      (apiposts: any[]) => {
+        for(let p of apiposts) {
+          this.posts.push(
+            new Post(p.nome, p.titulo, p.subtitulo, p.email, p.mensagem, p.arquivo, p.id, p.likes)
+          );
+        }
+      }
+    );
    }
 
 
